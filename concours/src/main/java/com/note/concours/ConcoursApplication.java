d26b92e9bb5a5c99a0d58c63a5464eb80b7e1078
Avotramarco <hei.marco2@gmail.com>
2023-08-29 16:00:57 +0200

    feat : initial project

---
+++ concours/src/main/java/com/note/concours/ConcoursApplication.java
@@ -0,0 +1,13 @@
+package com.note.concours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcoursApplication.class, args);
+	}
+
+}
---
+++ concours/src/test/java/com/note/concours/ConcoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.note.concours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
